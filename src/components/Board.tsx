6b1f5042d6000573c5acff81fc408a142f813430
Elise Lawrence <eliselorraine@Los-Mors-iMac.home>
2022-01-25 11:27:29 +0100

    some functionality

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,34 +4,62 @@ import Button from './Button';
 import { useState } from 'react';
 import { FaDivide, FaMinus, FaTimes, FaPlus, FaEquals } from 'react-icons/fa';
 
-// Do we want to add decimal?
-
 export const Board: React.FC = () => {
-    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.'];
     const [input, setInput] = useState<string>('');
+
+    const calculate = (arr: string[]): void => {
+        const number1 = parseFloat(arr[0]);
+        const number2 = parseFloat(arr[2]);
+        const answer = (number1 + number2).toString();
+        if(answer === 'NaN') {
+            setInput('ERROR');
+        }
+        setInput(answer);
+    }   
 
     const math = (e: React.MouseEvent) => {
         const clicked = e.target as Element;
+        const expression = input.split(' ');
         switch (clicked.id) {
             case "plus": {
-                setInput(input + '+');
+                if (expression.length  === 3) {
+                    calculate(expression);
+                    return;
+                }
+                setInput(input + ' + ');
                 break;
             }
             case "subtract": {
-                setInput(input + '-');
+                if(input.length > 0) {
+                    setInput(input + ' - ');
+                } else {
+                    setInput('-');
+                }
                 break;
             }
             case "multiply": {
-                setInput(input + '*');
+                setInput(input + ' * ');
                 break;
             }
             case "divide": {
-                setInput(input + '/');
+                setInput(input + ' / ');
                 break;
             }
             case "equalSign": {
-                console.log(input);
-                setInput(eval(input));
+                const expression = input.split(' ');
+                const number1 = parseFloat(expression[0]);
+                const number2 = parseFloat(expression[2]);
+                const answer = (number1 + number2).toString();
+                if(answer === 'NaN') {
+                    setInput('ERROR');
+                    break;
+                }
+                setInput(answer);
+                break;
+            }
+            case "clear": {
+                setInput('');
             }
         }
     }
@@ -44,24 +72,27 @@ export const Board: React.FC = () => {
                 </div>
                 <div className="board_container">
                     <div className="board_number-container">
+                        <button id="clear" className="clear-button number-button" onClick={math}>
+                            AC
+                        </button>
                         {numbers.map(element => {
-                            return <Button num={element} userInput={input} setInput={setInput} key={Math.floor(Math.random() * 10000)} />
+                            return <Button num={element} userInput={input} setInput={setInput} key={Math.floor(Math.random() * 100000)} />
                         })}
                     </div>
                     <div className="board_operator-container">
-                        <button id="plus" className="board_operator" onClick={math}>
+                        <button id="plus" className="board_operator add-button" onClick={math}>
                             <FaPlus id="plus" onClick={math} />
                         </button>
-                        <button className="board_operator" id="subtract" onClick={math}>
+                        <button className="board_operator subtract-button" id="subtract" onClick={math}>
                             <FaMinus id="subtract" onClick={math} />
                         </button>
-                        <button className="board_operator" id="multiply" onClick={math}>
+                        <button className="board_operator multiply-button" id="multiply" onClick={math}>
                             <FaTimes id="multiply2" onClick={math} />
                         </button>
-                        <button className="board_operator" id="divide" onClick={math}>
+                        <button className="board_operator divide-button" id="divide" onClick={math}>
                             <FaDivide id="divide" onClick={math} />
                         </button>
-                        <button id="equalSign" className="board_operator" onClick={math}>
+                        <button id="equalSign" className="board_operator equals-button" onClick={math}>
                             <FaEquals id="equalSign" onClick={math} />
                         </button>
                     </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,20 +2,30 @@ import React from 'react';
 import '../styles/Button.css'
 
 interface Props {
-    num: number;
+    num: number | string;
     userInput: string;
     setInput: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const Button: React.FC<Props> = (prop: Props) => {
+    const error = (): void => {
+        if(prop.userInput.length >= 5) {
+            prop.setInput('WHY');
+            return;
+        }
+    }
+
     const handleClick = (e: React.MouseEvent) => {
+        if (prop.userInput === 'ERROR') {
+        }
         const clicked = e.target as Element;
+        console.log(prop.userInput.split(' '));
         prop.setInput(prop.userInput + clicked.innerHTML);
     }
 
     return (
         <div className="button">
-            <button onClick={handleClick}>
+            <button onClick={handleClick} className="number-button">
                 {prop.num}
             </button>
         </div>
